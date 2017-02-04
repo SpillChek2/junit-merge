baefeac606e2ebd42b51ef3c2674b37330a2bbe8
Joseph W Becher <jwbecher@drazisil.com>
2017-02-03 23:01:43 -0500

    This should run, but hangs badly

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,32 +4,37 @@ var expect = require('chai').expect // eslint-disable-line no-unused-vars
 
 describe('File Handling', function () {
   describe('listXmlFiles()', function () {
-    it('should return 5 files', function () {
+    it('should return 5 files', function (done) {
       junitMerge.listXmlFiles('test/fixtures', function (err, res) {
         if (err) {
           throw err
         } else {
           res.length.should.equal(5)
+          done()
         }
       })
     })
 
-    it('should report not a valid directory', function () {
+    it('should report not a valid directory', function (done) {
       junitMerge.listXmlFiles('bad_dir', function (err, res) {
         if (err) {
           err.code.should.equal('ENOENT')
+          done()
         } else {
           res.should.equal('This should error')
+          done()
         }
       })
     })
 
-    it('should report not xml files found', function () {
+    it('should report not xml files found', function (done) {
       junitMerge.listXmlFiles('lib', function (err, res) {
         if (err) {
           err.should.equal('No xml files found')
+          done()
         } else {
           res.should.equal('This should error')
+          done()
         }
       })
     })
--- test/xml.spec.js
+++ test/xml.spec.js
@@ -1,9 +1,9 @@
 var chai = require('chai')
-const fs = require('fs')
 var junitMerge = require('../lib/index.js')
+var fs = require('fs') // eslint-disable-line no-unused-vars
 var should = chai.should() // eslint-disable-line no-unused-vars
 var expect = chai.expect // eslint-disable-line no-unused-vars
-chai.use(require('chai-xsd-schema'))
+var xsd = require('libxml-xsd')
 
 describe('XML Handling', function () {
   describe('getTestsuites()', function () {
@@ -15,72 +15,86 @@ describe('XML Handling', function () {
       })
     })
 
-    it('should return 1 testsuites', function () {
+    it('should return 1 testsuites', function (done) {
       junitMerge.getTestsuites('./test/fixtures/1.xml', function (err, res) {
         if (err) {
           throw err
         }
         junitMerge.testsuiteCount.should.equal(1)
+        done()
       })
     })
 
-    it('should return 1 testsuite', function () {
+    it('should return 1 testsuite', function (done) {
       junitMerge.getTestsuites('./test/fixtures/testcase-1.xml', function (err, res) {
         if (err) {
           throw err
         }
         junitMerge.testsuiteCount.should.equal(1)
+        done()
       })
     })
 
-    it('should return 2 testsuites', function () {
+    it('should return 2 testsuites', function (done) {
       junitMerge.getTestsuites('./test/fixtures/3.xml', function (err, res) {
         if (err) {
           throw err
         }
         junitMerge.testsuiteCount.should.equal(2)
+        done()
       })
     })
 
-    it('should return No tests found', function () {
+    it('should return No tests found', function (done) {
       junitMerge.mergeFiles(['./test/fixtures/empty.xml'], function (err, res) {
         if (err) {
           err.should.equal('No tests found')
+          done()
         } else {
           res.should.equal('This should error')
+          done()
         }
       })
     })
 
-    it('should return merged xml file', function () {
+    it('should return merged xml file', function (done) {
       junitMerge.mergeFiles(['./test/fixtures/1.xml', './test/fixtures/3.xml'], function (err, res) {
         if (err) {
           throw err
         }
         res.length.should.equal(852)
+        done()
       })
     })
 
-    it('should return File not found', function () {
+    it('should return File not found', function (done) {
       junitMerge.getTestsuites('./test/fixtures/12.xml', function (err, res) {
         if (err) {
           err.should.equal('File not found')
+          done()
         } else {
           res.should.equal(null)
+          done()
         }
       })
     })
 
-    it('Passes with valid input and valid schema', function () {
-      var schema = fs.readFileSync('test/fixtures/JUnit.xsd', {
-        encoding: 'utf8'
+    it('Passes with valid input and valid schema', function (done) {
+      var schema
+      xsd.parseFile('test/fixtures/JUnit.xsd', function (err, schemaObj) {
+        if (err) {
+          throw err
+        }
+        schema = schemaObj
+        schema.validateFile('merged-test-results2.xml', function (err, validationErrors) {
+          if (err) {
+            throw err
+          } else {
+            console.log('ICK')
+            done()
+          }
+        })
       })
-
-      var xml = fs.readFileSync('merged-test-results.xml', {
-        encoding: 'utf8'
-      })
-
-      expect(xml).to.be.validXML(schema)
     })
   })
 })
